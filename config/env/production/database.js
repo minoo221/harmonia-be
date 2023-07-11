d5f424b4caaa8f16868aede130f9efab2cc06073
Milan Blasko <milan.blasko@interway.sk>
2023-07-11 14:30:01 +0200

    production env

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,12 +2,18 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'harmonia'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'majo11'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: {
+            rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+      },
+      debug: false,
+      options: {
+          ssl: false
+        },
     },
   },
 });
