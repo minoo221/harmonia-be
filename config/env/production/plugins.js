d008d26279a05845705b4a022b0457100bfacec2
Milan Blasko <milan.blasko@interway.sk>
2023-07-27 09:27:03 +0200

    sendgrid implementation

---
+++ config/env/production/plugins.js
@@ -0,0 +1,50 @@
+module.exports = ({ env }) => ({
+	"users-permissions": {
+		config: {
+			register: {
+				allowedFields: ["nickname"],
+			},
+		},
+	},
+	email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'milanblasko28@gmail.com',
+        defaultReplyTo: 'milanblasko28@gmail.com',
+      },
+    },
+  },
+	ezforms:{
+		config:{
+			captchaProvider: {
+				name: 'none',
+			},
+			notificationProviders: [
+				{
+					provider: 'nodemailer',
+					enabled: true,
+					config: {
+						from: 'milanblasko28@gmail.com'
+					}
+				}
+			]
+		}
+	},
+	 upload: {
+		config: {
+		provider: "strapi-provider-upload-dos",
+		providerOptions: {
+			key: process.env.DO_SPACE_ACCESS_KEY,
+			secret: process.env.DO_SPACE_SECRET_KEY,
+			endpoint: process.env.DO_SPACE_ENDPOINT,
+			space: process.env.DO_SPACE_BUCKET,
+			directory: process.env.DO_SPACE_DIRECTORY,
+		},
+		},
+	},
+
+});
